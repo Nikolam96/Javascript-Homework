8e1ad5a5d24868cc6c86b5f12008376149e9dc55
Nikola Mitic <Nikolamitic1696@gmail.com>
2023-11-28 18:16:44 +0100

    adding D6

--- D6/1.js
+++ D6/1.js
@@ -7,28 +7,42 @@ window.onload = function(){
 
 //2
 let picking = document.querySelectorAll("#frm1 input")
-let piciking1 = Array.from(picking).reduce((acc, input) => ({...acc,[input.name]:input.value}),{});
+    let piciking1= Array.from(picking).reduce((acc, input) => ({...acc,[input.name]:input.value}),{})
 console.log(piciking1)
 
-//3
+// //3
 let pick2 = document.getElementsByTagName("input")
 console.log(pick2[0].value)
 
-//4
+// //4
 let myUsername = document.getElementsByClassName("username");
 console.log(myUsername[3].value);
 
-//5
+// //5
 for(let i=0;i<pick.length;i++){
-  console.log(document.querySelectorAll("#frm1 input")[i].name + ":")
-  console.log(document.querySelectorAll("#frm1 input")[i].value)
+  console.log(document.querySelectorAll("#frm1 input")[i].name + ":"+ document.querySelectorAll("#frm1 input")[i].value)
 }
 
-//6
+// //6
 document.querySelectorAll("#frm1 input")
 Array.from(picking).forEach(ele=>console.log(ele.name+": "+ele.value))
 
-//Mario
+//7
+  document.addEventListener("submit",(e)=>{e.preventDefault();
+  if ((pick2[0].value) == "" || (pick2[1].value) == "" || (pick2[2].value) == "" || (pick2[3].value) == ""){alert("Please fill the req")}
+     else{ 
+      for(let i=0;i<pick.length;i++){
+      console.log(document.querySelectorAll("#frm1 input")[i].name + ":"+ document.querySelectorAll("#frm1 input")[i].value)}
+  }})
+
+//8
+document.addEventListener("submit",(e)=>{e.preventDefault();
+let picking = document.querySelectorAll("#frm1 input")
+    let piciking1= Array.from(picking).reduce((acc, input) => ({...acc,[input.name]:input.value}),{})
+console.log(piciking1)
+})
+
+// //Mario
 for(let i=0;i<7;i++){
   let text = "#"
   console.log(text.repeat(i))
